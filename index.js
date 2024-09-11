280e0303d64c0309dfaa9378613e7f9758621876
fernandomg <fernando.greco@gmail.com>
2024-09-11 18:10:14 +0200

    refactor: make script compatible with windows systems

--- index.js
+++ index.js
@@ -1,74 +1,67 @@
 #!/usr/bin/env node
 
 const { execSync } = require("child_process");
-// const fs = require("fs");
 const path = require("path");
-// const readline = require("readline");
+const fs = require("fs");
+const os = require("os");
 
 const repoUrl = "https://github.com/bootnodedev/dappbooster.git";
 const projectName = process.argv[2];
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-try {
-  /^[a-zA-Z0-9-_]+/.test(projectName);
-  cloneRepo(projectName);
-  return;
-} catch (error) {
+if (!projectName || !/^[a-zA-Z0-9-_]+$/.test(projectName)) {
   console.error("Invalid directory name. Please enter a valid project name.");
   process.exit(1);
 }
 
-// request project name to continue
-// rl.question("Enter the project name: ", (projectName) => {
-//   rl.close();
-//   cloneRepo(projectName);
-// });
+cloneRepo(projectName);
+
+function execQuiet(command) {
+  const options = {
+    stdio: "pipe",
+    shell: true,
+  };
+
+  if (os.platform() === "win32") {
+    command = `${command} > nul 2>&1`;
+  } else {
+    command = `${command} > /dev/null 2>&1`;
+  }
+
+  return execSync(command, options);
+}
 
 function cloneRepo(projectName) {
-  // Sanitize the project name to create a valid directory name
   const sanitizedProjectName = projectName.replace(/[^a-zA-Z0-9-_]/g, "-");
   const projectDir = path.join(process.cwd(), sanitizedProjectName);
 
   try {
-    console.log(`cloning DappBooster...`);
+    console.log(`Cloning DappBooster...`);
 
-    // Clone the repository with minimum history
-    execSync(
-      `git clone --depth 1 --no-checkout "${repoUrl}" "${projectDir}" > /dev/null 2>&1`,
-      {
-        stdio: "inherit",
-      }
-    );
+    // Clone the repository
+    execQuiet(`git clone --depth 1 --no-checkout "${repoUrl}" "${projectDir}"`);
 
     // Change to the project directory
     process.chdir(projectDir);
 
     // Fetch all tags
-    execSync("git fetch --tags > /dev/null 2>&1", { stdio: "inherit" });
+    execQuiet("git fetch --tags");
 
     // Get the latest tag
     const latestTag = execSync(
-      "git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null"
+      "git describe --tags $(git rev-list --tags --max-count=1)"
     )
       .toString()
       .trim();
 
     // Checkout the latest tag
-    execSync(`git checkout "${latestTag}" > /dev/null 2>&1`, {
-      stdio: "inherit",
-    });
+    execQuiet(`git checkout "${latestTag}"`);
 
-    process.chdir(projectDir);
+    // Remove the .git directory
+    fs.rmSync(path.join(projectDir, ".git"), { recursive: true, force: true });
 
-    execSync("rm -rf .git", { stdio: "inherit" });
+    // Initialize a new git repository
+    execQuiet("git init");
 
-    execSync("git init > /dev/null 2>&1", { stdio: "inherit" });
-
-    // Print the checked out tag
     console.log(`DappBooster repository cloned in ${projectDir}`);
   } catch (error) {
     console.error("An error occurred:", error.message);
