8dae7dc4878dc0f87a31f8f767e59d874b34fac7
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-05-24 00:25:33 -0300

    feat: breakline component

--- source/import/BreakLine.tsx
+++ source/import/BreakLine.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Text, Newline } from 'ink'
 
-const BreakLine = () => {
-  return (
-    <Text>
-      <Newline />
-    </Text>
-  )
-}
+const BreakLine = () => (
+  <Text>
+    <Newline />
+  </Text>
+)
 
 export default BreakLine
