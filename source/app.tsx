965556bf66ce0e158c018ce32f1a0ad906b3f0ca
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-05-24 03:30:11 -0300

    feat: add steps to main

--- source/app.tsx
+++ source/app.tsx
@@ -10,7 +10,8 @@ import BigText from 'ink-big-text'
 const App = () => {
   const [projectName, setProjectName] = useState<string>('')
   const [errorMessage, setErrormessage] = useState<string | undefined>()
-  const answered = useMemo(
+  const [step, setStep] = useState(1)
+  const isProjectNameSet = useMemo(
     () => isAnswerConfirmed(projectName, errorMessage),
     [projectName, errorMessage],
   )
@@ -18,6 +19,8 @@ const App = () => {
   useEffect(() => {
     setErrormessage(validateName(projectName))
   }, [projectName])
+
+  const finishStep = () => setStep(step + 1)
 
   return (
     <Box flexDirection={'column'} rowGap={1}>
@@ -31,10 +34,25 @@ const App = () => {
         question={'Project name?'}
         tip={'Letters (a–z, A–Z), numbers (0–9), hyphens (-), and underscores (_) are allowed.'}
       />
-      {answered && (
+      {isProjectNameSet && (
         <>
-          <Divider titlePadding={2} titleColor={'whiteBright'} title={`Cloning "${projectName}"`} />
-          <CloneRepo projectName={projectName} />
+          {/* Step 1 */}
+          {step > 0 && (
+            <>
+              <Divider
+                titlePadding={2}
+                titleColor={'whiteBright'}
+                title={`Cloning "${projectName}"`}
+              />
+              <CloneRepo projectName={projectName} onCompletion={() => finishStep()} />
+            </>
+          )}
+          {/* Step 2 */}
+          {step > 1 && (
+            <>
+              <Divider titlePadding={2} titleColor={'whiteBright'} title={'Installation setup'} />
+            </>
+          )}
         </>
       )}
     </Box>
