925da473062e8578a3197d3bbb2133229686b992
Gabito Esmiapodo <4015436+gabitoesmiapodo@users.noreply.github.com>
2025-05-27 12:31:48 -0300

    refactor: move packages list to config file

--- source/import/constants/config.ts
+++ source/import/constants/config.ts
@@ -3,17 +3,22 @@ import type { ExecSyncOptions } from 'node:child_process'
 export const repoUrl = 'https://github.com/BootNodeDev/dAppBooster.git'
 export const homeFolder = '/src/components/pageComponents/home'
 
-export const defaultExecOptions: ExecSyncOptions = {
-  stdio: 'pipe',
-  shell: 'true',
-}
-
-export const fileExecOptions = {
-  recursive: true,
-  force: true,
-}
-
-export const installPackageExecOptions: ExecSyncOptions = {
-  stdio: 'inherit',
-  shell: 'true',
+export const featurePackages: {
+  [key: string]: string[]
+} = {
+  subgraph: [
+    '@bootnodedev/db-subgraph',
+    'graphql graphql-request',
+    '@graphql-codegen/cli',
+    '@graphql-typed-document-node/core',
+  ],
+  typedoc: [
+    'typedoc',
+    'typedoc-github-theme',
+    'typedoc-plugin-inline-sources',
+    'typedoc-plugin-missing-exports',
+    'typedoc-plugin-rename-defaults',
+  ],
+  vocs: ['vocs'],
+  husky: ['husky', 'lint-staged', '@commitlint/cli', '@commitlint/config-conventional'],
 }
